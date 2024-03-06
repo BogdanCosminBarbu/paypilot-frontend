dc2873de44e399bbb02f15062932af3e53a9fed7
ivona.racasean <ivona_racasean9@yahoo.com>
2024-03-06 13:17:24 +0200

    Added creation for payment schedule

---
+++ src/app/scheduled-payments/dtos/bill.ts
@@ -0,0 +1,8 @@
+export interface Bill{
+    id: number;
+    amount: number;
+    name: string;
+    due_date: string;
+    billing_cycle: string;
+    billCategory: string
+}
--- src/app/scheduled-payments/scheduled-payments.service.ts
+++ src/app/scheduled-payments/scheduled-payments.service.ts
@@ -1,16 +1,36 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ScheduledPaymentsService {
-  private baseUrl = 'http://yourbackendapi.com/scheduled-payments'; // Change to your backend URL
+  
+  private baseUrl = 'http://localhost:8082/scheduled-payment'; 
+  private billUrl = 'http://localhost:8082/bill/matching';
+  private userUrl = 'http://localhost:8082/user';
 
   constructor(private http: HttpClient) { }
 
   getScheduledPayments(): Observable<any> {
     return this.http.get(`${this.baseUrl}`);
+  }
+
+  getBills(category: string, dateFrom: string, dateTo: string): Observable<any> {
+    let params = new HttpParams()
+      .set('billCategory', category)
+      .set('dateFrom', dateFrom)
+      .set('dateTo', dateTo);
+  
+    return this.http.get(this.billUrl, { params });
+  }
+
+  getUserById(id: number): Observable<any> {
+    return this.http.get(`${this.userUrl}/${1}`);
+  }
+
+  createScheduledPayment(schPaymentData: any): Observable<any> {
+    return this.http.post(`${this.baseUrl}/create`, schPaymentData);
   }
 }
