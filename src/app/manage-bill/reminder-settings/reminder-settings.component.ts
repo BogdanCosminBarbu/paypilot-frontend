00d7635e6239265c8ac103b970979a11c98b69a3
VladGrigorita <79250115+VladGrigorita@users.noreply.github.com>
2024-03-07 10:32:51 +0200

    reminder-settings

--- src/app/manage-bill/reminder-settings/reminder-settings.component.ts
+++ src/app/manage-bill/reminder-settings/reminder-settings.component.ts
@@ -7,6 +7,7 @@ import { BillGroceriesService } from '../bill-overview/bill-groceries/bill-groce
 import { BillInternetService } from '../bill-overview/bill-internet/bill-internet.service';
 import { BillRetirementService } from '../bill-overview/bill-retirement/bill-retirement.service';
 import { ReminderSettings } from '../../notification/model/reminderSettings.model';
+import { AddBillService } from '../service/add-bill.service';
 
 @Component({
   selector: 'app-reminder-settings',
@@ -25,7 +26,11 @@ export class ReminderSettingsComponent implements OnInit{
     private billGroceriesService: BillGroceriesService,
     private billInternetService: BillInternetService,
     private billRetirementService : BillRetirementService,
-    ){}
+    private addBillService:AddBillService,
+    ){
+      this.bill = new Bill();
+      this.reminderSettings = new ReminderSettings();
+    }
 
   ngOnInit(){
 
@@ -60,6 +65,14 @@ export class ReminderSettingsComponent implements OnInit{
   }
 
   saveSettings(){
-    
+    this.reminderSettings.active = true;
+    this.bill.reminderSettings = new ReminderSettings();
+    this.bill.reminderSettings.active = this.reminderSettings.active;
+    this.bill.reminderSettings.message = this.reminderSettings.message;
+    this.bill.reminderSettings.reminderFrequency = this.reminderSettings.reminderFrequency;
+    this.bill.reminderSettings.reminderStartDate = this.reminderSettings.reminderStartDate;
+    this.addBillService.updateBill(this.bills[0],this.bills[0].billId).subscribe(data =>{
+      console.log("Bill updated");
+    })
   }
 }
